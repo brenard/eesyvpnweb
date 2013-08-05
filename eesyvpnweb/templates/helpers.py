5cee294599215dccdcea627de8348633267abee7
Benjamin Renard <bn8@zionetrix.net>
2013-08-06 00:36:21 +0200

    Added expire_date() template helper

--- eesyvpnweb/templates/helpers.py
+++ eesyvpnweb/templates/helpers.py
@@ -3,6 +3,7 @@
 
 import os
 import random
+import datetime
 
 
 def random_sequence(length):
@@ -11,3 +12,9 @@ def random_sequence(length):
 
 def relative_path(ctx, abs_path):
     return os.path.relpath(abs_path, ctx.req.path)
+
+def expire_date(date):
+	if date > datetime.datetime.now() and date < datetime.datetime.now()-datetime.timedelta(days=30):
+		return '<span class="badge badge-warning">%s</span>' % date
+	else:
+		return '<span class="badge badge-info">%s</span>' % date
