4830ba84fb54b7c980fc62c6cdcdc49ecbdbdbf5
Benjamin Renard <bn8@zionetrix.net>
2013-08-06 00:42:23 +0200

    Fix indent

--- eesyvpnweb/controllers.py
+++ eesyvpnweb/controllers.py
@@ -14,31 +14,31 @@ log = logging.getLogger(__name__)
 
 @wsgify
 def home(req):
-    return templates.render(req.ctx, '/home.mako', data={})
+	return templates.render(req.ctx, '/home.mako', data={})
 
 @wsgify
 def certs(req):
-    params = req.params
-    log.debug(u'params = {}'.format(params))
-    inputs = {
-        'type': params.get('type'),
-        'state': params.get('state'),
-        }
-    log.debug(u'inputs = {}'.format(inputs))
-    data, errors = conv.inputs_to_certs_data(inputs)
-    if errors is not None:
-        return wsgi_helpers.bad_request(req.ctx, comment=errors)
-        
-    log.debug(u'data = {}'.format(data))
-    
-    if data['type']=='client':
+	params = req.params
+	log.debug(u'params = {}'.format(params))
+	inputs = {
+		'type': params.get('type'),
+		'state': params.get('state'),
+		}
+	log.debug(u'inputs = {}'.format(inputs))
+	data, errors = conv.inputs_to_certs_data(inputs)
+	if errors is not None:
+		return wsgi_helpers.bad_request(req.ctx, comment=errors)
+		
+	log.debug(u'data = {}'.format(data))
+	
+	if data['type']=='client':
 		if data['state']=='valid':
 			data['certs']=eesyvpn.listValidCerts()
 			return templates.render(req.ctx, '/certs_client_valid.mako', data=data)
-    elif data['state']=='revoked':
+	elif data['state']=='revoked':
 		data['certs']=eesyvpn.listRevokedCerts()
 		return templates.render(req.ctx, '/certs_client_revoked.mako', data=data)
-    elif data['type']=='server':
+	elif data['type']=='server':
 		data['certs']=eesyvpn.listValidCerts(type='server')
 		return templates.render(req.ctx, '/certs_server.mako', data=data)
 
@@ -62,8 +62,8 @@ def cert(req):
     return templates.render(req.ctx, '/cert.mako', data=data)
 
 def make_router():
-    return router.make_router(
-        ('GET', '^/$', home),
-        ('GET', '^/certs$', certs),
-        ('GET', '^/cert$', cert),
-        )
+	return router.make_router(
+		('GET', '^/$', home),
+		('GET', '^/certs$', certs),
+		('GET', '^/cert$', cert),
+		)
